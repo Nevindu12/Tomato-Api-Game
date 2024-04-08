1f2d71c5895bebe5a4b91eee82ff4cc222f1008d
Nevindu12 <nilangithnevindu@gmail.com>
2024-04-09 00:17:41 +0530

    fetching the game

--- View/index.php
+++ View/index.php
@@ -1,6 +1,6 @@
 <?php
 include '../Controller/config.php';
-if(!$_SESSION['loggedIn']){
+if (!$_SESSION['loggedIn']) {
     redirect("login.php");
 }
 ?>
@@ -12,13 +12,11 @@ if(!$_SESSION['loggedIn']){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../Assets/Css/index.css" type="text/css">
+    <link rel="stylesheet" href="../Assets/Css/index.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
     <title>MATHZY QUIZ</title>
 </head>
@@ -28,8 +26,8 @@ if(!$_SESSION['loggedIn']){
     <nav class="navbar">
         <h1 class="logo">MATHZY QUIZ</h1>
         <div class="links">
-            <?php if($_SESSION['loggedIn']){ ?>
-                <a href="profile.php">Hi, <?=$_SESSION['user_username'];?></a>
+            <?php if ($_SESSION['loggedIn']) { ?>
+                <a href="profile.php">Hi, <?= $_SESSION['user_username']; ?></a>
             <?php } ?>
             <a href="scores.php"><i class="bi bi-trophy-fill"></i></a>
             <a href="../Controller/logout.php"><i class="bi bi-power custom-icon"></i></a>
@@ -39,12 +37,49 @@ if(!$_SESSION['loggedIn']){
         <div class="content">
             <h1 class="mainTitle">Let's Play</h1>
             <img id="image" src="../Assets/Images/main_img.png" alt="">
-            <?php if($_SESSION['loggedIn']){ ?>
-                <a href="howtoplay.php"><button class="howtoplayBtn" id="startbtn">How To Play</button></a>
-            <?php } ?>    
-            <a href="sample.html"><button class="startBtn" id="startbtn">Start Playing</button></a>
+            <?php if ($_SESSION['loggedIn']) { ?>
+                <a href="#"><button class="startBtn">Start Playing</button></a>
+            <?php } ?>
+            <a href="#"><button class="howtoplayBtn" id="howtoplayBtn">How To Play</button></a>
         </div>
     </div>
+
+    <div class="levelOut">
+        <div class="level">
+            <div class="close">X</div>
+            <p>Choose Difficulty</p>
+            <button id='easy'>Easy</button>
+            <button id='medium'>Medium</button>
+            <button id='hard'>Hard</button>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $('.startBtn').click(function() {
+                $('.levelOut').css('display', 'flex')
+                console.log('levels')
+            })
+            $('.close').click(function() {
+                $('.levelOut').css('display', 'none')
+            })
+            $('#easy').click(function() {
+                localStorage.setItem('livesLeft', '6');
+                localStorage.setItem('timeLeft', '120');
+                window.location.href = "./ingame.php";
+            })
+            $('#medium').click(function() {
+                localStorage.setItem('livesLeft', '4');
+                localStorage.setItem('timeLeft', '80');
+                window.location.href = "./ingame.php";
+            })
+            $('#hard').click(function() {
+                localStorage.setItem('livesLeft', '3');
+                localStorage.setItem('timeLeft', '15');
+                window.location.href = "./ingame.php";
+            })
+        })
+    </script>
 
 </body>
 
---
+++ View/ingame.php
@@ -0,0 +1,286 @@
+<?php
+include '../Controller/config.php';
+if (!$_SESSION['loggedIn']) {
+    redirect("login.php");
+}
+
+if (isset($_GET['new'])) {
+    echo '<script>localStorage.removeItem("timeLeft");</script>';
+    echo '<script>localStorage.removeItem("score");</script>';
+    echo '<script>localStorage.removeItem("numQuestions");</script>';
+    echo '<script>localStorage.removeItem("currentLevel");</script>';
+
+    echo '<script>const currentURL = new URL(window.location.href);</script>';
+    echo '<script>const searchParams = new URLSearchParams(currentURL.search);</script>';
+    echo '<script>searchParams.delete("new");</script>';
+    echo '<script>history.replaceState({}, "", ${ currentURL.pathname } ? ${ searchParams.toString() });</script>';
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="../Photos/icon.ico" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link rel="stylesheet" href="../Assets/Css/ingame.css">
+
+    <title>MATHZY QUIZ</title>
+    <script>
+        let timeLeft = localStorage.getItem('timeLeft');
+        let score = localStorage.getItem('score') || 0;
+        let numQuestions = localStorage.getItem('numQuestions') || 1;
+        let currentLevel = localStorage.getItem('currentLevel') || 1;
+        let livesLeft = localStorage.getItem('livesLeft');
+        let timer;
+        let imgApi;
+        let solution;
+        let timerInterval;
+
+        function updateUI() {
+            document.getElementById("question-number").textContent = numQuestions;
+            document.getElementById("score").textContent = score;
+            document.getElementById("timer").textContent = timeLeft;
+            document.getElementById("level-no").textContent = currentLevel;
+        }
+
+
+
+        function fetchImage() {
+            fetch('https://marcconrad.com/uob/tomato/api.php')
+                .then(response => response.json())
+                .then(data => {
+                    imgApi = data.question;
+                    solution = data.solution;
+                    document.getElementById("imgApi").src = imgApi;
+
+                })
+                .catch(error => {
+                    console.error('Error fetching image from the API:', error);
+                });
+        }
+
+
+        //setting green to correct answer
+        function correctAns(ans) {
+            ans.css('background-color', '#00E142')
+            setTimeout(() => {
+                ans.css('background-color', 'rgb(39, 39, 39)')
+            }, 1000);
+        }
+        //setting red to wrong answer
+        function wrongAns(ans) {
+            ans.css('background-color', 'rgb(255, 43, 43)')
+            setTimeout(() => {
+                ans.css('background-color', 'rgb(39, 39, 39)')
+            }, 1000);
+        }
+        //updating lives
+        function lives() {
+            $(".lives").html("<img src='../Assets/Images/heart.gif'>".repeat(livesLeft));
+            if (timeLeft == -1) {
+                countDown()
+            }
+        }
+
+
+        //Game over
+        function gameOver() {
+
+            if (livesLeft == 0) {
+                var audioElement = document.getElementById("over");
+                var audio = document.getElementById("myAudio");
+                audio.pause();
+                audioElement.play();
+                document.getElementById("clock").pause();
+                $('.timeTag').removeClass('active');
+                $('.overOut').css('display', 'flex');
+            }
+        }
+
+
+        //Time Countdown
+        function countDown() {
+            clearInterval(timerInterval);
+            timeLeft = localStorage.getItem('timeLeft');
+            timerInterval = setInterval(() => {
+                $("#timer").text(timeLeft);
+
+                if (timeLeft <= 10) {
+                    $('.timeTag').addClass('active');
+                    document.getElementById("clock").play();
+                }
+
+                timeLeft -= 1;
+
+                if (timeLeft < 0) {
+                    clearInterval(timerInterval);
+                    $('.timeTag').removeClass('active');
+                    livesLeft -= 1;
+                    lives();
+                    document.getElementById("wrong").play();
+                    document.getElementById("clock").pause();
+                    // console.log(livesLeft)
+                    fetchImage();
+                }
+                if (livesLeft == 0) {
+                    clearInterval(timerInterval);
+                    document.getElementById("clock").pause();
+                    gameOver();
+                }
+            }, 1000);
+        }
+
+
+
+
+        //High Score
+        function highScore(score) {
+            var dataSet = {
+                score: score,
+                userName: $('#userName').text()
+            };
+
+            $.ajax({
+
+                type: 'POST',
+                url: '../server/highScore.php',
+                data: dataSet,
+                success: function (response) {
+
+                    if (response === 'Success') {
+                        //console.log(response);
+                    }
+                    console.log(response);
+                },
+                error: function () {
+                    alert('Error occurred. Please try again.');
+                }
+
+            });
+        }
+
+        document.addEventListener("DOMContentLoaded", function () {
+            updateUI();
+            fetchImage();
+            lives();
+        });
+
+        $(document).ready(function () {
+
+            countDown();
+
+            $(".ansBtn").click(function (e) {
+                var userAns = $(this).text();
+                if (userAns == solution) {
+                    correctAns($(this))
+                    document.getElementById("correct").play();
+                    //document.getElementById("note").innerHTML = 'Correct';
+                    score += 10;
+                    highScore(score)
+                    numQuestions += 1;
+                    fetchImage();
+                    updateUI();
+                    countDown();
+                    $('.timeTag').removeClass('active');
+                    document.getElementById("clock").pause();
+                }
+                else {
+                    wrongAns($(this))
+                    var audioElement = document.getElementById("wrong");
+                    audioElement.currentTime = 0;
+                    audioElement.play();
+
+                    livesLeft -= 1;
+                    lives();
+                    gameOver();
+                }
+            })
+
+            $('#again').click(function (e) {
+                $('.overOut').css('display', 'none');
+            })
+
+            //in game audio
+            $('#ingame').click(function (e) {
+                var audio = document.getElementById("myAudio");
+
+                if (audio.paused) {
+                    audio.play();
+                    $(this).html("<i class='bi bi-volume-mute'></i>");
+                    console.log('play')
+                }
+                else {
+                    audio.pause();
+                    $(this).html("<i class='bi bi-music-note'></i>");
+                    console.log('pause')
+                }
+
+            })
+
+        })
+    </script>
+</head>
+
+<body>
+    <div class="maincontainer">
+        <nav class="navbar">
+            <h1 class="logo">MATHZY QUIZ</h1>
+            <div class="links">
+                <!-- <a href="#" id="ingame"><i class="bi bi-volume-mute"></i></a> -->
+                <a href="index.php"><i class="bi bi-house-fill custom-icon"></i></a>
+                <a href="scores.php"><i class="bi bi-trophy-fill custom-icon"></i></a>
+                <!-- <a href="profile.php"><i class="bi bi-person-fill custom-icon"></i></i></a> -->
+                <a href="../Controller/logout.php"><i class="bi bi-power custom-icon"></i></a>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="uTitle">Hi <span id='userName'>
+                    <?php echo "" . $_SESSION["user_username"] . ""; ?>
+                </span>, LET'S PLAY! </div>
+
+            <div class="single-Data">
+                <span>Level <span id="level-no">1</span></span>
+                <span>Question<span id="question-number">1</span></span>
+                <span>Score<span id="score">0</span></span>
+                <span class="timeTag" id='timeTag'>Time(S) <span id="timer">45</span></span>
+                <div class="lives">
+                </div>
+            </div>
+            <div class="imgApi">
+                <img src="" alt="Question Image" id="imgApi" class="color-image">
+            </div>
+
+            <div class="ans-align">
+                <p class="txtAns" id='ans'>Choose The Answer : <button class='ansBtn'>0</button> <button
+                        class='ansBtn'>1</button> <button class='ansBtn'>2</button> <button class='ansBtn'>3</button>
+                    <button class='ansBtn'>4</button>
+                    <button class='ansBtn'>5</button> <button class='ansBtn'>6</button> <button
+                        class='ansBtn'>7</button> <button class='ansBtn'>8</button> <button class='ansBtn'>9</button>
+                </p>
+            </div>
+
+            <div class="overOut">
+                <div class="over">
+                    <p>GAME OVER</p>
+                    <a href="" id="again"><button>Play Again</button></a>
+                    <a href="./index.php"><button> Home</button></a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
